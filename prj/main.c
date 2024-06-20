5f5a7684acd9b7148f53ff46cb75ea4f9ea02083
Cyitron <gclencina@hotmail.com>
2024-06-20 17:18:33 -0300

    atualizacao do doc e do mplab code

--- prj/main.c
+++ prj/main.c
@@ -16,13 +16,17 @@
 #include <xc.h>
 #define _XTAL_FREQ 4000000
 
-// NOTE: To use the macros below, YOU must have previously defined _XTAL_FREQ
- //#define __delay_us(x) _delay((unsigned long)((x)*(_XTAL_FREQ/4000000.0)))
- //#define __delay_ms(x) _delay((unsigned long)((x)*(_XTAL_FREQ/4000.0)))
  void main(void) {
-    //76543210
-    TRISB = 0b00000000;
+    
+    // clear ports
+    PORTA = 0b00000000;
     PORTB = 0b00000000;
+    PORTC = 0b00000000;
+    
+    // set outputs and inputs
+    TRISB = 0b00010010;
+    TRISA = 0b00001111;
+    TRISC = 0b00000000;
     while(1){
     //PORTBbits.RB0 = 1;
     //__delay_ms(1000);
@@ -32,3 +36,54 @@
     __delay_ms(500);
     }
  }
+ 
+ /*
+  RA0,RA3 = CCP1 INPUT
+  RA1,RA2 = CCP2 INPUT
+  RC1 = CCP1 OUTPUT
+  RC2 = CCP2 OUTPUT
+  RB1 = ECHO1
+  RB2 = TRIG1
+  RB4 = ECHO2
+  RB5 = TRIG2
+  */
+ 
+//------------------------------
+/* Código feito no ARDUINO (validado)
+#define trigPin 10
+#define echoPin 13
+
+void setup() {
+  Serial.begin (9600);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+}
+
+void loop() {
+  float duration, distance;
+  digitalWrite(trigPin, LOW); 
+  delayMicroseconds(2);
+ 
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  
+  duration = pulseIn(echoPin, HIGH);
+  distance = (duration / 2) * 0.0344;
+  
+  if (distance >= 400 || distance <= 2){
+    Serial.print("Distance = ");
+    Serial.println("Out of range");
+  }
+  else {
+    Serial.print("Distance = ");
+    Serial.print(distance);
+    Serial.println(" cm");
+    delay(500);
+  }
+  delay(500);
+}
+ */
+ 
+ //#define __delay_us(x) _delay((unsigned long)((x)*(_XTAL_FREQ/4000000.0)))
+ //#define __delay_ms(x) _delay((unsigned long)((x)*(_XTAL_FREQ/4000.0)))
